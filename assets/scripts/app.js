c156a4c66643216419c8c6d89f945b99c44ce6e2
Will <willj4945@gmail.com>
2023-04-17 16:39:17 -0400

    Removed unused function.

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -5,39 +5,33 @@ const PLAYER_HEAL_VALUE = 10;
 const MONSTER_ATTACK_VALUE = 14;
 let playerMaxLife = 100;
 let monsterMaxLife = 100;
-let playerCurrentLife = playerMaxLife
+let playerCurrentLife = playerMaxLife;
 let monsterCurrentLife = monsterMaxLife;
 
 adjustHealthBars(playerMaxLife);
 
-function gameOverCheck () {
-    if (monsterCurrentLife <= 0 && playerCurrentLife > 0){
-        alert("Player wins!")
-    } else if (playerCurrentLife <= 0 && monsterCurrentLife > 0){
-        alert("Monsrer wins!")
-    }else if(playerCurrentLife <= 0 && monsterCurrentLife <=0){
-        alert("Tie!")
-    }
-
+function gameOverCheck() {
+  if (monsterCurrentLife <= 0 && playerCurrentLife > 0) {
+    alert("Player wins!");
+  } else if (playerCurrentLife <= 0 && monsterCurrentLife > 0) {
+    alert("Monsrer wins!");
+  } else if (playerCurrentLife <= 0 && monsterCurrentLife <= 0) {
+    alert("Tie!");
+  }
 }
 
-function playerWins () {
-  
+function attackHandler() {
+  const damage = dealMonsterDamage(ATTACK_VALUE);
+  monsterCurrentLife -= damage;
+
+  const heavyDamage = dealMonsterDamage(STRONG_ATTACK_VALUE);
+  monsterCurrentLife -= heavyDamage;
+
+  const playerRecievedDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE);
+  playerCurrentLife -= playerRecievedDamage;
+
+  gameOverCheck();
 }
 
-function attackHandler ( ) {
-
-    const damage = dealMonsterDamage(ATTACK_VALUE)
-    monsterCurrentLife -=damage
-
-    const heavyDamage = dealMonsterDamage(STRONG_ATTACK_VALUE)
-    monsterCurrentLife -= heavyDamage
-    
-    const playerRecievedDamage = dealPlayerDamage(MONSTER_ATTACK_VALUE)
-    playerCurrentLife -= playerRecievedDamage
-    
-    gameOverCheck();
-}
-
-attackBtn.addEventListener('click',attackHandler);
-strongAttackBtn.addEventListener('click',attackHandler);
+attackBtn.addEventListener("click", attackHandler);
+strongAttackBtn.addEventListener("click", attackHandler);
